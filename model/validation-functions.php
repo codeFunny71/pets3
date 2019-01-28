7fe5b99c21ee26876150d4dca64043d0524bc520
Marcus Absher <mabsher@mail.greenriver.edu>
2019-01-28 15:07:56 -0500

    worked on validation

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -9,3 +9,17 @@ function validColor($color){
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+function validString($animal){
+   // global $f3;
+    if (empty($valid)){
+        echo "Enter an animal";
+        return false;
+    }
+    if (!preg_match("/^[a-zA-Z ]*$/",$animal)) {
+        echo "Only letters and white space allowed";
+        return false;
+    }
+    return true;
+
+}
